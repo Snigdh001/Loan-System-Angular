241c4a3df41edf8e60c78fe6dfe08eedee251bfa
Snigdh Gupta <guptasnigdh510@gmail.com>
2023-02-09 16:03:26 +0530

    09/02/2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:`<input >
+  //   <button class="btn btn-primary">click</button>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-app';
+  asd = false;
+  constructor(){
+    setTimeout(()=>{this.asd=true},2000)
+  }
+  onClickHandler(evt:Event){
+    console.log(evt)
+  }
 }
